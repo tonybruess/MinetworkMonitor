ffd57df2fb1534f2f707f71fb503569f7cf8eb06
mrapple <tony@oc.tc>
2013-08-01 23:51:44 -0500

    More deploy fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,18 +24,5 @@ set :rails_env, :production
 
 ssh_options[:forward_agent] = true
 
-namespace :deploy do
-    namespace :assets do
-        task :precompile, :roles => :web, :except => { :no_release => true } do
-            from = source.next_revision(current_revision)
-            if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-                run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-            else
-                logger.info "Skipping asset pre-compilation because there were no asset changes"
-            end
-        end
-    end
-end
-
-require 'capistrano-unicorn'
-after "deploy:restart", "unicorn:restart"
+#require 'capistrano-unicorn'
+#after "deploy:restart", "unicorn:restart"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 MinetworkMonitor::Application.routes.draw do
     root :to => 'application#index'
-
-    match ':controller(/:action(/:id(.:format)))'
 end
